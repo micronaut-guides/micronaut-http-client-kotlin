27b95da57d9de75923f7384d617e8607b6f45048
sdelamo <sergio.delamo@softamo.com>
2018-08-22 15:38:19 +0200

    Bump up kotlinVersion to 1.2.61 and spek version to 2.0.0-alpha.1

--- complete/src/test/kotlin/example/micronaut/BintrayFilterConditionSpec.kt
+++ complete/src/test/kotlin/example/micronaut/BintrayFilterConditionSpec.kt
@@ -1,18 +1,18 @@
 package example.micronaut
 
-import org.jetbrains.spek.api.Spek
-import org.jetbrains.spek.api.dsl.describe
-import org.jetbrains.spek.api.dsl.on
-import org.junit.jupiter.api.Assertions
 import io.micronaut.context.ApplicationContext
 import io.micronaut.context.exceptions.NoSuchBeanException
+import org.spekframework.spek2.Spek
+import org.spekframework.spek2.style.specification.describe
+import kotlin.test.assertNotNull
+import kotlin.test.assertTrue
 
 class BintrayFilterConditionSpec: Spek({
 
     describe("BintrayFilter is loaded") {
         val applicationContext = ApplicationContext.run(mapOf<String, Any>(Pair("bintray.username", "john"), Pair("bintray.token", "XXX")), "test")
-         on("Verify BintrayFilter is loaded if bintray.username/bintray.token are set") {
-             Assertions.assertNotNull(applicationContext.getBean(BintrayFilter::class.java))
+         it("Verify BintrayFilter is loaded if bintray.username/bintray.token are set") {
+             assertNotNull(applicationContext.getBean(BintrayFilter::class.java))
         }
         afterGroup {
             applicationContext.close()
@@ -21,14 +21,14 @@ class BintrayFilterConditionSpec: Spek({
 
     describe("BintrayFilter is not loaded") {
         val applicationContext = ApplicationContext.run("test")
-        on("Verify BintrayFilter is NOT loaded if bintray.username and bintray.token are not set") {
+        it("Verify BintrayFilter is NOT loaded if bintray.username and bintray.token are not set") {
             var exceptionThrown = false
             try {
                 applicationContext.getBean(BintrayFilter::class.java)
             } catch (e: NoSuchBeanException) {
                 exceptionThrown = true
             }
-            Assertions.assertTrue(exceptionThrown)
+            assertTrue(exceptionThrown)
         }
         afterGroup {
             applicationContext.close()
